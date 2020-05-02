3c8b5a486a6c2528a2691993bec6ebbe4868f33d
jackokramer <jackokramer@gmail.com>
2020-05-02 09:32:52 -0700

    stup

--- day_19/script.js
+++ day_19/script.js
@@ -1,4 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle')
-const nav = document.querySelector('click', ()=>{
-    nav.classList.toggle('nav--visible');
+const nav = document.querySelector('.nav');
+
+navToggle.addEventListener('click', ()=>{
+    nav.classList.toggle('nav-visible');
 })
